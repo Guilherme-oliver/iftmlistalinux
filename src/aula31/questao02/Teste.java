d8493052381beed7b853058dda8c58399fb0894c
Guilherme-oliver <guilherme.rodriguess.oliveira@gmail.com>
2022-07-13 20:46:25 -0300

    Atualização

--- src/aula31/questao02/Teste.java
+++ src/aula31/questao02/Teste.java
@@ -5,9 +5,9 @@ package aula31.questao02;
 public class Teste {
     public static void main(String[] args) {
 
-      Assalariado assalariado = new Assalariado("Joao","MG741", 1500);
-      Comissionado comissionado = new Comissionado("Pedro","MG852",1000000,1);
-      Horista horista = new Horista("Philip","MG951",80,180);
+      Empregado assalariado = new Assalariado("Joao","MG741", 1500);
+      Empregado comissionado = new Comissionado("Pedro","MG852",1000000,1);
+      Empregado horista = new Horista("Philip","MG951",80,180);
 
         System.out.println(assalariado);
         System.out.println(comissionado);
--- src/aula31/questao03/Teste.java
+++ src/aula31/questao03/Teste.java
@@ -2,8 +2,8 @@ package aula31.questao03;
 
 public class Teste {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente("Paulo","Master");
-        Secretaria secretaria = new Secretaria("beatriz", "*101*");
+        Funcionario gerente = new Gerente("Paulo","Master");
+        Funcionario secretaria = new Secretaria("beatriz", "*101*");
 
         secretaria.trabalha();
         gerente.trabalha();
