3d679c9e8b526bfdb155072a18da07602f5d33d8
Guilherme-oliver <guilherme.rodriguess.oliveira@gmail.com>
2022-07-13 12:13:46 -0300

    Atualização

--- src/aula31/questao01/Teste.java
+++ src/aula31/questao01/Teste.java
@@ -2,8 +2,8 @@ package aula31.questao01;
 
 public class Teste {
     public static void main(String[] args) {
-        Leao leao = new Leao(250,"Mufaza");
-        Vaca vaca = new Vaca("Nelore", "Vacão");
+        Animal leao = new Leao(250,"Mufaza");
+        Animal vaca = new Vaca("Nelore", "Vacão");
 
         vaca.fala();
         vaca.come();
